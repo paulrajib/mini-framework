3782ffb3457a79d0e5c3807c6d8153c53305a777
root <ashfaq1701@gmail.com>
2017-09-12 14:56:38 +0600

    added model superclass

---
+++ app/core/model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Core;
+
+class Model {
+  protected $massAssignable = [];
+
+  public function fromArray(array $data)
+  {
+    foreach ($data as $key => $value) {
+      if(in_array($key, $this->massAssignable)) {
+        $this->key = $value;
+      }
+    }
+  }
+}
